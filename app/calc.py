1abcea19d44221879ed753dccdbed4be1fe46b4f
smzb <smzb@mitos-kalandiel.me>
2019-09-05 01:14:19 +0100

    some more module-package-naming changes, added a py linter into my virtualenv (mostly for use in emacs, but one should be able to call it via flake8 when in the virtualenv

--- app/calc.py
+++ app/calc.py
@@ -6,68 +6,70 @@ __author__= "Sebastian Meier zu Biesen"
 __copyright__= "2000-2019 by MzB Solutions"
 __email__= "smzb@mitos-kalandiel.me"
 
-def add(num1, num2):
-    """This functions adds two numbers"""
-    return num1 + num2
+class Calculator():
 
-def subtract(num1, num2):
-    """This is a simple subtraction function"""
-    return num1 - num2
+    def add(num1, num2):
+        """This functions adds two numbers"""
+        return num1 + num2
 
-def multiply(num1, num2):
-    """Again a simple multiplication"""
-    return num1 * num2
+    def subtract(num1, num2):
+        """This is a simple subtraction function"""
+        return num1 - num2
 
-def divide(num1, num2):
-    """division function
-    todo: (smzb/js) make division by 0 impossible"""
-    return num1 / num2
+    def multiply(num1, num2):
+        """Again a simple multiplication"""
+        return num1 * num2
 
-def ask_op():
-    """Lets ask what the user wants to do"""
-    print("Please select operation -\n"
-        "1. Add\n"
-        "2. Subtract\n"
-        "3. Multiply\n"
-        "4. Divide\n")
+    def divide(num1, num2):
+        """division function
+        todo: (smzb/js) make division by 0 impossible"""
+        return num1 / num2
 
-    # Take input from the user
-    op = input("Select operations form 1, 2, 3, 4 :")
-    return op
+    def ask_op():
+        """Lets ask what the user wants to do"""
+        print("Please select operation -\n"
+              "1. Add\n"
+              "2. Subtract\n"
+              "3. Multiply\n"
+              "4. Divide\n")
 
-def ask_number():
-    """Get a number from the user"""
-    num = int(input("Enter an operand: ")) 
-    return num
+        # Take input from the user
+        op = input("Select operations form 1, 2, 3, 4 :")
+        return op
 
-"""Some debugging"""
-DO_DEBUG=1
+    def ask_number():
+        """Get a number from the user"""
+        num = int(input("Enter an operand: ")) 
+        return num
 
-"""Lets start by getting all necessary information"""
-operation=ask_op()
-num1=ask_number()
-num2=ask_number()
+    """Some debugging"""
+    DO_DEBUG=1
 
-if DO_DEBUG:
-    print("Number 1: ", num1,
-          "Number 2: ", num2,
-          "operation :", operation)
+    """Lets start by getting all necessary information"""
+    operation=ask_op()
+    num1=ask_number()
+    num2=ask_number()
 
-"""Now evaluate what operation the user wants, and run the consecutive function"""
-if operation == '1':
-    print(num1, "+", num2, "=",
-                    add(num1, num2))
+    if DO_DEBUG:
+        print("Number 1: ", num1,
+              "Number 2: ", num2,
+              "operation :", operation)
 
-elif operation == '2':
-    print(num1, "-", num2, "=",
-                    subtract(num1, num2))
+    """Now evaluate what operation the user wants, and run the consecutive function"""
+    if operation == '1':
+        print(num1, "+", num2, "=",
+              add(num1, num2))
 
-elif operation == '3':
-    print(num1, "*", num2, "=",
-                    multiply(num1, num2))
+    elif operation == '2':
+        print(num1, "-", num2, "=",
+              subtract(num1, num2))
 
-elif operation == '4':
-    print(num1, "/", num2, "=",
-                    divide(num1, num2))
-else:
-    print("Invalid operation")
+    elif operation == '3':
+        print(num1, "*", num2, "=",
+              multiply(num1, num2))
+
+    elif operation == '4':
+        print(num1, "/", num2, "=",
+              divide(num1, num2))
+    else:
+        print("Invalid operation")
--- test/test_calc.py
+++ test/test_calc.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
+"""These are simply unit tests to demonstrate the facility
+This is by no means meant to be comprehensive"""
 import unittest
 
+__author__= "Sebastian Meier zu Biesen"
+__copyright__= "2000-2019 by MzB Solutions"
+__email__= "smzb@mitos-kalandiel.me"
+
+class Test_Calc(unittest.TestCase):
+
+    def test_add_returns_correct_result(self):
+        calc = Calculator()
